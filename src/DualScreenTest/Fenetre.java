da040dbe09fdd139264736b9442e8738b4a5de03
RomainPastureau <romain.pastureau@u-bordeaux.fr>
2017-06-16 15:11:22 +0200

    RolyPoly DualScreen Test 0.2
    Ajout d'un outil de détection d'écran.

--- src/DualScreenTest/Fenetre.java
+++ src/DualScreenTest/Fenetre.java
@@ -12,6 +12,8 @@ import java.awt.event.MouseListener;
 import java.awt.geom.Rectangle2D;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import Shared.PressKey;
 import TUIO.TuioBlob;
@@ -33,14 +35,26 @@ public class Fenetre extends JFrame implements MouseListener, KeyListener, TuioL
 	public Fenetre(){
 		
 		Rectangle2D result = new Rectangle2D.Double();
-		GraphicsEnvironment localGE = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		for (GraphicsDevice gd : localGE.getScreenDevices()) {
-		  for (GraphicsConfiguration graphicsConfiguration : gd.getConfigurations()) {
-			System.out.println(graphicsConfiguration.getBounds());
-		    result.union(result, graphicsConfiguration.getBounds(), result);
-		  }
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice[] gs = ge.getScreenDevices();
+		JOptionPane.showConfirmDialog((java.awt.Component) null, "Found : "+gs.length, "screen detected ?", JOptionPane.DEFAULT_OPTION);
+		for(int j = 0; j < gs.length; j++){
+			GraphicsDevice gd = gs[j];
+			JFrame frame = new JFrame(gd.getDefaultConfiguration());
+			frame.setTitle("I'm on monitor #"+j);
+			frame.setSize(400,200);
+			frame.add(new JLabel(""+j));
+			frame.setVisible(true);
 		}
-		System.out.println("RESULT: "+result);
+		
+//		System.out.println(gds[0].getDisplayModes());
+//		for (GraphicsDevice gd : localGE.getScreenDevices()) {
+//		  for (GraphicsConfiguration graphicsConfiguration : gd.getConfigurations()) {
+//			System.out.println(graphicsConfiguration.getBounds());
+//		    result.union(result, graphicsConfiguration.getBounds(), result);
+//		  }
+//		}
+//		System.out.println("RESULT: "+result);
 		//f.setSize(result.getWidth(), result.getHeight());
 //		//Titre de fenêtre
 //		this.setTitle("RolyPoly MultiTouch Test 1.6");
