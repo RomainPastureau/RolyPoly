9d44a420cb055979a88de27bc29198d05e6da86c
RomainPastureau <romain.pastureau@u-bordeaux.fr>
2017-06-19 14:24:39 +0200

    RolyPoly DualScreen Test 0.7
    Nouvel essai sur les sockets.

--- src/DualScreenTest/Client.java
+++ src/DualScreenTest/Client.java
@@ -1,17 +1,51 @@
 package DualScreenTest;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 	
 	public static void main(String[] args) {
 		
 		Socket socket;
+		int x = 0;
+		String server = "";
+		BufferedWriter out;
 		
 		try{
-			socket = new Socket("localhost", 4242);
+			System.out.println("Veuillez sélectionner un serveur :");
+			System.out.println("1. 141.115.72.8 (climber)");
+			System.out.println("2. 141.115.72.18 (immer)");
+			Scanner sc = new Scanner(System.in);
+			
+			while(x != 1 && x != 2){
+				System.out.println("Votre choix : ");
+				x = sc.nextInt();
+				if(x != 1 && x != 2){
+					System.out.println("Erreur, veuillez entrer un nouveau numéro.");
+				}
+			}
+			
+			switch(x){
+				case 1: server = "141.115.72.8";
+				case 2: server = "141.115.72.18";
+			}
+			
+			socket = new Socket(server, 4242);
+			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			
+			while(x != -1){
+				x = sc.nextInt();
+				out.write(x);
+			}
+			
 			socket.close();
+			
 		} catch(IOException e){
 			e.printStackTrace();
 		}
--- src/DualScreenTest/Serveur.java
+++ src/DualScreenTest/Serveur.java
@@ -1,6 +1,9 @@
 package DualScreenTest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,7 +17,7 @@ public class Serveur {
 			socket = new ServerSocket(4242);
 			Thread t = new Thread(new Accepter_clients(socket));
 			t.start();
-			System.out.println("Mes employeurs sont prêts !");
+			System.out.println("Serveur prêt.");
 			
 		} catch(IOException e){
 			e.printStackTrace();
@@ -25,7 +28,10 @@ public class Serveur {
 class Accepter_clients implements Runnable {
 	private ServerSocket socketserver;
 	private Socket socket;
-	private int nbrclient = 1;
+	private int nbClient = 1;
+	protected int toAdd = 0;
+	protected int sum = 0;
+	protected BufferedReader in;
 	
 	public Accepter_clients(ServerSocket s){
 		socketserver = s;
@@ -36,8 +42,13 @@ class Accepter_clients implements Runnable {
 		try{
 			while(true){
 				socket = socketserver.accept();
-				System.out.println("Le client numéro "+nbrclient+" est connecté !");
-				nbrclient++;
+				InetAddress name = socket.getInetAddress();
+				System.out.println("Connexion du client n°"+nbClient+" : "+name);
+				nbClient++;
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				toAdd = Integer.valueOf(in.read()); 
+				sum += toAdd;
+				System.out.println(sum-toAdd+" + "+toAdd+" = "+sum);
 				socket.close();
 			}
 		} catch(IOException e){
