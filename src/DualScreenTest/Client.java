35644f6f739850589f33767a540fbffabf5c5493
RomainPastureau <romain.pastureau@u-bordeaux.fr>
2017-06-19 13:57:14 +0200

    RolyPoly DualScreen Test 0.6
    Modifications sur les sockets.

--- src/DualScreenTest/Client.java
+++ src/DualScreenTest/Client.java
@@ -1,10 +1,7 @@
 package DualScreenTest;
 
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
 import java.io.IOException;
+import java.net.Socket;
 
 public class Client {
 	
@@ -13,10 +10,8 @@ public class Client {
 		Socket socket;
 		
 		try{
-			socket = new Socket(InetAddress.getLocalHost(), 4242);
+			socket = new Socket("localhost", 4242);
 			socket.close();
-		} catch(UnknownHostException e){
-			e.printStackTrace();
 		} catch(IOException e){
 			e.printStackTrace();
 		}
--- src/DualScreenTest/Serveur.java
+++ src/DualScreenTest/Serveur.java
@@ -8,17 +8,41 @@ public class Serveur {
 
 	public static void main(String[] args) {  
 		
-		ServerSocket socketserver;
-		Socket socketduserveur;
+		ServerSocket socket;
 		
 		try{
-			socketserver = new ServerSocket(4242, 2);
-			socketduserveur = socketserver.accept();
-			System.out.println("Connexion ok !");
-			socketserver.close();
-			socketduserveur.close();
+			socket = new ServerSocket(4242);
+			Thread t = new Thread(new Accepter_clients(socket));
+			t.start();
+			System.out.println("Mes employeurs sont prêts !");
+			
 		} catch(IOException e){
 			e.printStackTrace();
 		}
 	}
+}
+
+class Accepter_clients implements Runnable {
+	private ServerSocket socketserver;
+	private Socket socket;
+	private int nbrclient = 1;
+	
+	public Accepter_clients(ServerSocket s){
+		socketserver = s;
+	}
+	
+	public void run(){
+		
+		try{
+			while(true){
+				socket = socketserver.accept();
+				System.out.println("Le client numéro "+nbrclient+" est connecté !");
+				nbrclient++;
+				socket.close();
+			}
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
 }
